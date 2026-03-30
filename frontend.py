306dc8da867139b41df37018b1e70266db00a585
Syedathar23 <nitaspirant81@gmail.com>
2026-03-30 10:01:21 +0530

    Done

--- frontend.py
+++ frontend.py
@@ -104,8 +104,8 @@ def feed_page():
     st.title("🏠 Feed")
 
     response = requests.get(f"{BASE_URL}/feed", headers=get_headers())  # ✅ fixed /feed -> /data
-    st.write(response.status_code)  # ← add this temporarily
-    st.write(response.json())
+    # ← add this temporarily
+
     if response.status_code == 200:
         posts = response.json()["posts"]
 
